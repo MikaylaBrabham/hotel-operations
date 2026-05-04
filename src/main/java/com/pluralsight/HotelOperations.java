20aaf5b488dcc033ad9a091af7a5c704189131ec
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-04 13:38:03 -0500

    added different classes outlined in guide

--- src/main/java/com/pluralsight/HotelOperations.java
+++ src/main/java/com/pluralsight/HotelOperations.java
@@ -5,4 +5,17 @@ public class HotelOperations {
     public static void main(String[] args) {
 
     }
+
+    // add room class
+private class Room {
+
+}
+//add Reservation class
+private class Reservation{
+
+}
+//add Employee Class
+private class Employee{
+
+}
 }
