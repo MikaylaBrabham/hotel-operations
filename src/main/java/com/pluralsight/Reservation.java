4dc35d22ac7683bd49d09f98f08f1c0cc052b09c
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-04 18:10:00 -0500

    add getters and setters from fixed stored attributes

--- src/main/java/com/pluralsight/Reservation.java
+++ src/main/java/com/pluralsight/Reservation.java
@@ -5,15 +5,48 @@ public class Reservation {
         private String roomType;
         private double price;
         private int numberOfNights;
-        private boolean weekend;
+        private boolean isWeekend;
         //private double reservationTotal; = derived
 
     //add constructors
-    public Reservation(String roomType, double price, int numberOfNights, boolean weekend) {
+    public Reservation(String roomType, double price, int numberOfNights, boolean isWeekend) {
         this.roomType = roomType;
         this.price = price;
         this.numberOfNights = numberOfNights;
-        this.weekend = weekend;
+        this.isWeekend = isWeekend;
     }
+
     //add get to stored constructors
+    public String getRoomType() {
+            return roomType;
+        }
+    public double getPrice() {
+        return price;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+
+    //add set to stored constructors
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public void setIsWeekend(boolean isWeekend) {
+        this.isWeekend = isWeekend;
+    }
 }
