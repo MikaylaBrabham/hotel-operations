9af28ec94cfee492fea7bfd38195ffae5e14cb54
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-04 17:40:40 -0500

    add constructors and fixed stored attributes

--- src/main/java/com/pluralsight/Reservation.java
+++ src/main/java/com/pluralsight/Reservation.java
@@ -1,44 +1,19 @@
 package com.pluralsight;
 //add Reservation class
 public class Reservation {
-        //add outlined classes
-        private String RoomType;
+        //add outlined stored method attributes
+        private String roomType;
         private double price;
         private int numberOfNights;
         private boolean weekend;
-        private double reservationTotal;
+        //private double reservationTotal; = derived
 
-        //add reservation constructors
-        public Reservation(String roomType, double price, int numberOfNights, boolean weekend, double reservationTotal) {
-            RoomType = roomType =("king" || "double");
-            this.price = price;
-            this.numberOfNights = numberOfNights;
-            this.weekend = weekend;
-            this.reservationTotal = reservationTotal;
-        }
-        //add getters
-
-        public String getRoomType() {
-            return RoomType;
-        }
-
-        public double getPrice() {
-            return price;
-        }
-
-        public int getNumberOfNights() {
-            return numberOfNights;
-        }
-
-        public boolean isWeekend() {
-            return weekend;
-        }
-
-        public double getReservationTotal() {
-            return reservationTotal;
-        }
-
-
-        //add setters
+    //add constructors
+    public Reservation(String roomType, double price, int numberOfNights, boolean weekend) {
+        this.roomType = roomType;
+        this.price = price;
+        this.numberOfNights = numberOfNights;
+        this.weekend = weekend;
     }
-    }
+    //add get to stored constructors
+}
