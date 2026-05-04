667026256dd3fbfc9be6a94224e492481ae298b5
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-04 13:47:22 -0500

    added constructor and defined variables

--- src/main/java/com/pluralsight/HotelOperations.java
+++ src/main/java/com/pluralsight/HotelOperations.java
@@ -8,8 +8,42 @@ public class HotelOperations {
 
     // add room class
 private class Room {
+        //add variables and their data properties
+        private double NumberOfBeds;
+        private double price;
+        private boolean occupied;
+        private boolean dirty;
+        private boolean available;
 
-}
+        //add getters
+        public double getNumberOfBeds() {
+            return NumberOfBeds;
+        }
+        public double getPrice() {
+            return price;
+        }
+
+        public boolean isOccupied() {
+            return occupied;
+        }
+
+        public boolean isDirty() {
+            return dirty;
+        }
+
+        public boolean isAvailable() {
+            return available;
+        }
+
+        // add constructors empty ones
+        public Room(double numberOfBeds, double price, boolean occupied, boolean dirty, boolean available) {
+            NumberOfBeds = numberOfBeds;
+            this.price = price;
+            this.occupied = occupied;
+            this.dirty = dirty;
+            this.available = available;
+        }
+    }
 //add Reservation class
 private class Reservation{
 
