18884a8d233f2032b0808bd929455bd606a1bf23
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-06 02:28:40 -0500

    added both constructors the wording is confusing. it's either this way or that the i need to call all variables in the first one and set those to zero

--- src/main/java/com/pluralsight/Hotel.java
+++ src/main/java/com/pluralsight/Hotel.java
@@ -7,7 +7,8 @@ public class Hotel {
     private String name;
 
     //keeps up with type od room
-    private boolean type;
+    private boolean isSuite;
+    private boolean isRoom;
     private int numberOfSuites;
     private int numberOfRooms;
     private int bookedSuites;
@@ -15,13 +16,62 @@ public class Hotel {
 
 
     //set1 = number suites & rooms
-    public Hotel(String name, boolean type, int numberOfSuites, int numberOfRooms) {
+    public Hotel(String name, boolean isSuite,boolean isRoom, int numberOfSuites, int numberOfRooms)
+    {
         this.name = name;
-        this.type = type;
-        this.numberOfSuites = numberOfSuites = 0;
-        this.numberOfRooms = numberOfRooms = 0;
+        this.isSuite = isSuite;
+        this.isRoom = isRoom;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+        this.bookedSuites = bookedSuites;
+        this.bookedBasicRooms = bookedBasicRooms;
+
+    }
+
+    //add private setter
+
+    public void setNumberOfRooms(int numberOfRooms) {
+        this.numberOfRooms = numberOfRooms;
+    }
+
+    public void setNumberOfSuites(int numberOfSuites) {
+        this.numberOfSuites = numberOfSuites;
+    }
+
+    public void setRoom(boolean room) {
+        isRoom = room;
+    }
+
+    public void setSuite(boolean suite) {
+        isSuite = suite;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
 
+
+
     //set2 = spec # of bookedsuites and bookedbasicrooms
+    public Hotel(String name, boolean isSuite,boolean isRoom, int numberOfSuites, int numberOfRooms, int bookedSuites, int bookedBasicRooms) {
+        this.name = name;
+        this.isSuite = isSuite;
+        this.isRoom = isRoom;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+        this.bookedSuites = bookedSuites = 0;
+        this.bookedBasicRooms = bookedBasicRooms = 0;
+    }
+
+    //add private setter
+
+
+    public void setBookedSuites(int bookedSuites) {
+        this.bookedSuites = bookedSuites;
+    }
+
+    public void setBookedBasicRooms(int bookedBasicRooms) {
+        this.bookedBasicRooms = bookedBasicRooms;
+    }
 }
