121b81d618b6379326476110ad50a3c191868d61
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-06 22:30:56 -0500

    - fixed strings - added constructor 1 - added constructor 2 - added getters - fixed main issue of 2nd constructor returning error. not sure why it fixed when I took out } on line 22 and added it back * no option to commit and push eb=ven though using same file and i'm logged in

--- src/main/java/com/pluralsight/Hotel.java
+++ src/main/java/com/pluralsight/Hotel.java
@@ -5,73 +5,51 @@ public class Hotel {
 
     // stored for first
     private String name;
-
-    //keeps up with type od room
-    private boolean isSuite;
-    private boolean isRoom;
     private int numberOfSuites;
     private int numberOfRooms;
     private int bookedSuites;
     private int bookedBasicRooms;
 
 
-    //set1 = number suites & rooms
-    public Hotel(String name, boolean isSuite,boolean isRoom, int numberOfSuites, int numberOfRooms)
-    {
+//set1 = number suites & rooms constructor
+
+    public Hotel(String name, int numberOfSuites, int numberOfRooms) {
         this.name = name;
-        this.isSuite = isSuite;
-        this.isRoom = isRoom;
-        this.numberOfSuites = numberOfSuites;
-        this.numberOfRooms = numberOfRooms;
-        this.bookedSuites = bookedSuites;
-        this.bookedBasicRooms = bookedBasicRooms;
-
-    }
-
-    //add private setter
-
-    public void setNumberOfRooms(int numberOfRooms) {
-        this.numberOfRooms = numberOfRooms;
-    }
-
-    public void setNumberOfSuites(int numberOfSuites) {
-        this.numberOfSuites = numberOfSuites;
-    }
-
-    public void setRoom(boolean room) {
-        isRoom = room;
-    }
-
-    public void setSuite(boolean suite) {
-        isSuite = suite;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
-
-
-    //set2 = spec # of bookedsuites and bookedbasicrooms
-    public Hotel(String name, boolean isSuite,boolean isRoom, int numberOfSuites, int numberOfRooms, int bookedSuites, int bookedBasicRooms) {
-        this.name = name;
-        this.isSuite = isSuite;
-        this.isRoom = isRoom;
         this.numberOfSuites = numberOfSuites;
         this.numberOfRooms = numberOfRooms;
         this.bookedSuites = bookedSuites = 0;
         this.bookedBasicRooms = bookedBasicRooms = 0;
     }
 
-    //add private setter
-
-
-    public void setBookedSuites(int bookedSuites) {
+    //set 2 constructors
+    public Hotel(String name, int numberOfSuites, int numberOfRooms, int bookedSuites, int bookedBasicRooms) {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
         this.bookedSuites = bookedSuites;
-    }
-
-    public void setBookedBasicRooms(int bookedBasicRooms) {
         this.bookedBasicRooms = bookedBasicRooms;
     }
+// add getters
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfSuites() {
+        return numberOfSuites;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public int getBookedSuites() {
+        return bookedSuites;
+    }
+
+    public int getBookedBasicRooms() {
+        return bookedBasicRooms;
+    }
 }
+
+
+
